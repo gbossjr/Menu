2f0020325c356972eeb1d182cb3eda987d1ded88
Geoffrey Bosire Jr <gbosirejr@gmail.com>
2020-04-28 05:11:16 -0800

    first commit

---
+++ app/src/main/java/android/example/menu/MainActivity.java
@@ -0,0 +1,24 @@
+package android.example.menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void printToLogs(View view) {
+        // Find first menu item TextView and print the text to the logs
+
+        // Find second menu item TextView and print the text to the logs
+
+        // Find third menu item TextView and print the text to the logs
+
+    }
+}
