9837bb99e54ca5f37626668d4fcfa0014935a413
Geoffrey Bosire Jr <gbosirejr@gmail.com>
2020-04-28 05:25:13 -0800

    added Textview item1 to print to the log

--- app/src/main/java/android/example/menu/MainActivity.java
+++ app/src/main/java/android/example/menu/MainActivity.java
@@ -3,7 +3,9 @@ package android.example.menu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void printToLogs(View view) {
         // Find first menu item TextView and print the text to the logs
+        TextView textItem1 = findViewById(R.id.menu_item_1);
+        String menueItem1 = textItem1.getText().toString();
+        Log.v("MainActivity",menueItem1);
 
         // Find second menu item TextView and print the text to the logs
 
